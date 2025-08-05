9bf6c8513e77f9bba334dbe5106c614906692d86
Abhay Sharma <abhaysharma20dec@gmail.com>
2025-08-05 18:03:16 +0530

    kk

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -51,7 +51,7 @@ export const login = async (req, res) => {
     if (!isPasswordValid)
       return res.status(400).json({ message: "Invalid Password!" });
 
-    // GENERATE COOKIE TOKEN AND SEND TO THE USER
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER *
 
     // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
     const age = 1000 * 60 * 60 * 24 * 7;
